db431d941834307db602e9b33103fe61709785c9
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2022-01-06 17:56:13 +0900

    카드 2 추가

--- src/components/card/Card2.vue
+++ src/components/card/Card2.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="card2">
-   
-    <p1>카드 2이에용</p1>
+   <div class="comment"> 
+    <div style="font-size:40px; line-height:50px">
+     <span>Short stories to</span><br/>
+     <span>feel, live, do.</span><br/>
+    </div><br/><br><br/>
+     <div style="font-size:20px; line-height:1.3em">
+      <span>우리에게 가장 소중한 경험들이 언제나 그렇듯이,</span><br/>
+      <span>단편선에서의 여정은 한 줄의 이야기로부터 시작됩니다.</span><br/><br/>
+      <span>여러분과 저희가 공유하는 이 공간에서, 우리는 함께</span><br/>
+      <span>그 이야기를 느끼고, 살고, 써내려가게 됩니다.</span><br/><br/>
+      <span>설레는 마음으로, 언제나 기다리고 있겠습니다.</span><br/><br/>
+      <span>2021년 5월 17일,</span><br/>
+      <span>단편선 드림</span>
+    </div>
+   </div>
 
   </div>
 </template>
@@ -24,8 +37,17 @@ export default {
 <style>
   .card2{
     width: 100%;
-    height: 100px;
-    background-color: rgba(126, 212, 133, 0.747);
+    height: 800px;
+    background-color: rgb(2, 52, 63);
+    padding: 180px;
 
+  }
+  .comment span{
+    font-weight: bold;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    padding: 0;
+    margin: 0;
+    text-align: left;
+    color:rgb(240, 237, 204);
   }
 </style>
