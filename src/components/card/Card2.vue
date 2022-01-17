b22dbde35445a2cc6891c597bd1dd390da902fd3
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2022-01-17 11:24:49 +0900

    Card2의 description 부분을 배열과 반목문으로 구현 변경

--- src/components/card/Card2.vue
+++ src/components/card/Card2.vue
@@ -7,13 +7,9 @@
         <span>{{theme.krTitle}}</span>
       </template>
       <template v-slot:content>
-        <span>우리에게 가장 소중한 경험들이 언제나 그렇듯이,</span><br/>
-        <span>단편선에서의 여정은 한 줄의 이야기로부터 시작됩니다.</span><br/><br/>
-        <span>여러분과 저희가 공유하는 이 공간에서, 우리는 함께</span><br/>
-        <span>그 이야기를 느끼고, 살고, 써내려가게 됩니다.</span><br/><br/>
-        <span>설레는 마음으로, 언제나 기다리고 있겠습니다.</span><br/><br/>
-        <span>2021년 5월 17일,</span><br/>
-        <span>단편선 드림</span>
+        <div class="description" :key="index" v-for="(description, index) in theme.descriptions">
+          <span :key="i" v-for="(text,i) in description">{{text}}<br/></span>
+        </div>  
       </template>    
     </Theme>  
 </template>
@@ -31,6 +27,13 @@ export default {
          krTitle: 'feel, live, do.',
          backgroundColor: 'rgb(2, 52, 63)',
          fontColor: 'rgb(240, 237, 204)',
+         descriptions: [
+           ['우리에게 가장 소중한 경험들이 언제나 그렇듯이,', '단편선에서의 여정은 한 줄의 이야기로부터 시작됩니다.'], 
+           ['여러분과 저희가 공유하는 이 공간에서, 우리는 함께', '그 이야기를 느끼고, 살고, 써내려가게 됩니다.'],
+           ['설레는 마음으로, 언제나 기다리고 있겠습니다.'],
+           ['2021년 5월 17일,', '단편선 드림']
+
+         ]
       }
     };
   },
@@ -52,6 +55,11 @@ export default {
     margin: auto;
     margin-left: 22%;
   }
+
+  .description{
+    margin-bottom: 2.5rem;
+  }
+
   .comment span{
     font-weight: bold;
     font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
