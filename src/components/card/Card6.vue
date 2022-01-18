8f734a5ff0c409fbfe4981b53b936e8ddd751c40
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2022-01-18 15:16:27 +0900

    카드6 슬롯 단순화

--- src/components/card/Card6.vue
+++ src/components/card/Card6.vue
@@ -1,44 +1,44 @@
 <template>
   <div>
-    <Theme :theme=theme>
-      <template v-slot:egTitle >
-         <span>{{theme.enTitle}}</span><br/>
-      </template>
-      <template v-slot:krTitle>
-          <span>{{theme.krTitle}}</span>
-      </template>
-      <template v-slot:content>
-        <span>석우는 연기를 들이키며 영원이라는 것에 대해 생각해보았다.</span><br/>
-        <span>아무래도 불행하겠지, 라고 생각하며 날숨.</span><br/>
-        <span>영원히 이어지는 게 고작 이런 삶이라면, 하며 틱.</span><br/><br/>
-        <span>손을 털며, 그는 오늘의 근무지의 간판을 올려다 보았다.</span><br/><br/>
-        <span>루드베키아.</span><br/><br/>
-        <span>영생을 발명했다는 기묘한 바이오 회사.</span><br/>
-        <span>공식 발표 이틀 전 사라져버린 연구소장.</span><br/>
-        <span>그 사건에 배정된 형사, 김석우.</span><br/><br/>
-        <span>석우는 루드베키아의 문을 열었다.</span>
-      </template>
-      <template v-slot:button>
-        <ReservationButton :theme=theme></ReservationButton>
-      </template>        
-    </Theme>  
+    <Theme :theme=theme />
   </div>
 </template>
 <script>
 
 import Theme from "@/components/slot/Theme"
-import ReservationButton from "@/components/slot/ReservationButton"
 
 export default {
   name: '',
-  components: { Theme,ReservationButton },
+  components: { Theme },
   data() {
     return {
         theme:{
-         enTitle: 'We decided to call it happiness',
-         krTitle: '사람들은 그것을 행복이라 부르기로 했다.',
-         backgroundColor: 'rgb(106, 123, 162)',
-         fontColor: '#FFDFDE',
+          head:{
+            title: [
+              'We decided to call it happiness',
+              '사람들은 그것을 행복이라 부르기로 했다.',
+            ],
+            titleFontColor : '',
+            titleFontSize : '',
+          },
+          content: {
+            descriptions: [
+              ['우리에게 가장 소중한 경험들이 언제나 그렇듯이,', '단편선에서의 여정은 한 줄의 이야기로부터 시작됩니다.'], 
+              ['여러분과 저희가 공유하는 이 공간에서, 우리는 함께', '그 이야기를 느끼고, 살고, 써내려가게 됩니다.'],
+              ['설레는 마음으로, 언제나 기다리고 있겠습니다.'],
+              ['2021년 5월 17일,', '단편선 드림']
+              ],
+              contentFontColor : '',
+              contentFontSize : '',
+          },
+          button: {
+             isRequiredReservationButtion: true,
+             buttonFontColor: '',
+             buttonBorderColor: '',
+          },
+           // Default 
+          backgroundColor: 'rgb(106, 123, 162)',
+          defaultColor: '#FFDFDE', 
       }
     };
   },
