a3f2a8b482e9d2b01b3296dc6b38685c6622d18e
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2022-02-14 10:38:47 +0900

    헤더의 스크롤 이벤트 추가 (색상은 추후 변경 예정)

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,45 +1,94 @@
 <template>
-  <header>
-      <div class="main"><a href="#">dpsnnn</a></div>
-      <ul class="menu">
-        <li><a href="#">단편선</a></li>
-        <li><a href="#">예약하기</a></li>
-        <li><a href="#">첫번째 이야기</a></li>
-        <li><a href="#">두번째 이야기</a></li>
-        <li><a href="#">Plans & Pricing</a></li>
-       </ul> 
+  <header :class="{headerActive : scrollPosition > 100}">
+      <!-- <div :class="{headerNoActive : scrollPosition > 182}"> -->
+        <div class="main"><a href="#">dpsnnn</a></div>
+        <div :class="{headerNoActive : scrollPosition > 182}">
+          <ul class="menu" >
+            <li><a href="#">단편선</a></li>
+            <li><a href="#">예약하기</a></li>
+            <li><a href="#">첫번째 이야기</a></li>
+            <li><a href="#">두번째 이야기</a></li>
+            <li><a href="#">Plans & Pricing</a></li>
+          </ul> 
+        </div>
+      <!-- </div> -->
   </header>  
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+
+    const header = document.querySelector(".header");
+
+    return {
+      scrollPosition : 0,
+    
+    }
+  },
+  mounted(){
+    window.scrollTo(0,0);
+    document.addEventListener('scroll', this.scrollEvents);
+  },
+  unmounted() {
+    document.removeEventListener('scroll', this.scrollEvents);
+  },
+
+  methods:{
+    
+    scrollEvents(){
+      this.scrollPosition = window.scrollY;
+      console.log(this.scrollPosition);
+      
+      // if(this.scrollPosition > 100){
+      //   this.isScrolled = true
+      // }else if( this.scrollPosition == 0){
+      //   this.isScrolled = false      
+      // }
+
+    }
+    
+  }
+
+};
 </script>
 
 <style scoped>
 header{
   width: 100%;
-  background-color: transparent;
+  /* background-color: transparent; */
+  background-color: #2c2524;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding: 70px;
+  padding: 15px;
   overflow: hidden;
   position: fixed;
   z-index: 1000;
+  transition: all 0.8s;
+  visibility: visible;
 }
-header ul{
+
+.headerActive{
+  background-color: transparent;
+  visibility: hidden;
+  /* background-color: rgb(247, 247, 247); */
+  transition: all 0.8s;
+}
+
+.menu ul{
   list-style: none;
   display: flex;
   justify-content: space-between;
 }
 
-header li a{
+.menu li a{
   text-align: center;
   padding: 18px;
   color: white;
   text-decoration: none;
 }
-header li a:hover{
+.menu li a:hover{
   text-decoration: none;
   color:cadetblue;
 }
@@ -56,7 +105,12 @@ header li a:hover{
   color:rgb(240, 216, 187);
 }
 .menu{
-  float: right;
+  
+  padding-top: 0.8rem;
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  
 }
 @media (max-width: 768px) {
   .menu{
