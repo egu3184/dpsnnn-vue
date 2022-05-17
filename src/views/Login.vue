0534abcc66c6e79379e7096ee8795d0f57c1f962
DESKTOP-VEPRU35\김성민 <zcvbnm90@naver.com>
2022-05-17 17:32:06 +0900

    Login.vue의 html 인라인 css 제거 / 모바일 대응 css 추가 /  오류 메시지 표현 방식을 기존의 alert가 아닌 메시지가 담긴 헤더 박스로 변경   / 가입 직후 veux의 값에 따라 헤더박스에 가입 완료 메시지 보이게, destoryed시 vuex값 초기화되도록 추가 / 일반 로그인시 정규표현식과 빈값 체크하는 메소드 가독성 높이고 재활용 가능하도록 구성과 순서 수정  / 빈값 체크는 패스워드에만 사용하도록 변경 / 상태에 따라 헤더박스의 css와 메시지를 set할 수 있는 메서드 추가 / id가 signup 페이지와 같이 email_id로 변경 /

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,31 @@
 <template>
     <div class="login_constructor">
+        <transition name="slide-fade" mode="out-in" v-if="showHeaderBox">
+        <div class="pop_box" :style="pop_box" style="font-size: 1.85rem; text-align:center;">
+                <b-icon  :icon=pop_box.icon></b-icon>&nbsp;&nbsp;
+                <span>{{pop_box.message}}</span>
+        </div>
+        </transition>       
         <div class="login_container">
             <div style="flex-direction: column; display:flex; align-items: center; justify-content: center;">
                 <form>
                 <div>
-                    <b-form-group label="아이디"  style="font-size: 1.8rem; width: 39rem; font-weight:bold;" label-for="name-input">
+                    <b-form-group class="label" label="이메일" label-for="name-input">
                         <b-input-group class="mb-2">
-                            <b-input-group-prepend is-text>
-                                <b-icon icon="person-fill"></b-icon>
+                            <b-input-group-prepend is-text style="width:3rem;">
+                                <b-icon icon="envelope-fill" class="b_icon"></b-icon>
                             </b-input-group-prepend>
-                            <b-form-input type="text" v-model="id" style="height: 4.5rem; font-size: 2rem;" placeholder="me@exaple.com"></b-form-input>
+                            <b-form-input maxlength="35" type="text" v-model="email_id" @blur="idBlur" class="input" placeholder="me@exaple.com"></b-form-input>
                         </b-input-group>
                     </b-form-group>
                 </div>
                 <div>
-                    <b-form-group label="비밀번호" style="font-size: 1.8rem; width: 39rem; font-weight:bold;" label-for="name-input">
+                    <b-form-group label="비밀번호" maxlength="20" class="label" label-for="name-input">
                         <b-input-group class="mb-2">
                             <b-input-group-prepend is-text>
-                                <b-icon icon="lock-fill"></b-icon>
+                                <b-icon icon="lock-fill" class="b_icon"></b-icon>
                             </b-input-group-prepend>
-                            <b-form-input type="password" v-model="pw" style="height: 4.5rem; font-size: 2rem;" placeholder="Enter your password"></b-form-input>
+                            <b-form-input v-model="pw" type="password" class="input" placeholder="Enter your password" ></b-form-input>
                         </b-input-group>
                     </b-form-group>
                 </div>
@@ -27,11 +33,13 @@
             </div>
             <div>      
                 <div style="display:flex; justify-content: center; ">
-                    <b-button class="mt-3" variant="primary" block @click="login" style="height: 4.8rem; font-size:1.8rem; font-weight:bold; width: 39rem; margin:2rem 0rem !important;" >Login</b-button>
+                    <b-button class="mt-3 button" variant="primary" block @click="login">Login</b-button>
                 </div>
-                <div style="display:flex; justify-content: center; margin:2rem 0rem; width:100%; font-size: 1.3rem;"> 
+                <div class="sign"> 
                     <div>
-                        <a hef="#" style=" olor: #007bff;">회원가입</a> / <a hef="#" style=" olor: #007bff;">아이디·비밀번호 찾기</a>
+                        <router-link to="/signup" style="text-decoration: none;">회원가입</router-link>
+                            / 
+                         <router-link to="/" style="text-decoration: none;">아이디·비밀번호 찾기</router-link>
                     </div>
                 </div> 
                 <div class="orElse" style="margin:2rem 0rem; font-size: 1.3rem;">
@@ -39,14 +47,14 @@
                 </div>
                 <div class="socialIconLine" style="width:100%">
                     <GoogleLogin class="google-login-button socialIcon" :params="googleParams" 
-                             b :onSuccess="googleLoginSuccess" :onFailure="googleLoginFailure" style="border:0; padding: 0; margin: 0rem 2rem;">
-                        <a ><img src="@/assets/Icon_google.jpg" style="height: 4.5rem; width: 4.5rem;"  /></a>
+                             b :onSuccess="googleLoginSuccess" :onFailure="googleLoginFailure">
+                        <a ><img src="@/assets/Icon_google.jpg" class="icon_image"  /></a>
                     </GoogleLogin>
-                    <div @click="getNaverAuthorizeUrl" class="socialIcon"  style="margin: 0rem 2rem;">
-                        <a href="#"><img src="@/assets/Icon_naver.png"  style="height: 4.25rem; width: 4.25rem;"  /></a>
+                    <div @click="getNaverAuthorizeUrl" class="socialIcon">
+                        <a href="#"><img src="@/assets/Icon_naver.png"  class="icon_image"/></a>
                     </div>
-                    <div @click="kakaoLogin" class="socialIcon"  style="margin: 0rem 2rem;"> 
-                        <a href="#"><img src="@/assets/Icon_kakao.jpg"  style="height: 4.5rem; width: 4.5rem;"  /></a>
+                    <div @click="kakaoLogin" class="socialIcon"> 
+                        <a href="#"><img src="@/assets/Icon_kakao.jpg"  class="icon_image"/></a>
                     </div>
                     <div id="buttonDiv"></div> 
                 </div>
@@ -89,12 +97,21 @@ export default {
       },
       ChangeloginIconSrc: false,
       //로그인 관련
-      id: '',
+      email_id: '',
       pw: '',
       isLogin : '',
       googleParams:{
           client_id: "829453067412-qi7uk2f6rd408agq4dtrl92v0qqamkic.apps.googleusercontent.com"
       },
+      showHeaderBox: false,
+      pop_box:{
+        "icon": "",
+        "border": "",
+        "background-color":"",
+         message: "",
+      }
+
+
     }
   },
   mounted(){
@@ -102,17 +119,21 @@ export default {
     this.searchUrlParams();
     //회원가입 직후 가입 완료 메시지 보이기
     if(this.$store.state.CompleteSignup == true){
-
+        this.showHeaderBox = true
+        this.setPopbox_completeSignup();
     }    
   },
-  unmounted() {
+  destroyed() {
     //스크롤 이벤트 리스너 제거
     document.removeEventListener('scroll', this.scrollEvents);
+
+    //vuex 가입 완료 state값 초기화
+    this.$store.commit("setCompleteSignup", false);
   },
+
   methods:{
     //구글 로그인 성공 메서드
     googleLoginSuccess(googleUser){
-        console.log(googleUser)
         axios({
             url:"http://localhost:2030/social/login/"+"google",
             method:"post",
@@ -131,7 +152,7 @@ export default {
     },
     //구글 로그인 실패 메서드
     googleLoginFailure(){
-        this.$store.commit("alert_Error", "로그인이 실패하였습니다.");
+        this.setPopbox_error("구글 로그인이 실패하였습니다.")
     },
     //네이버 인가 코드 요청 url  
     getNaverAuthorizeUrl(){
@@ -169,7 +190,7 @@ export default {
                 location.href = "/";
             }
         }).catch((error)=>{
-             this.$store.commit("alert_Error", "로그인이 실패하였습니다.");
+              this.setPopbox_error("네이버 로그인이 실패하였습니다.")
         });
     },
     //카카오 로그인 후 Jwt 토큰 받아오기
@@ -189,11 +210,11 @@ export default {
                         location.href = "/";
                   }
                 }).catch((err)=>{
-                     this.$store.commit("alert_Error", "로그인이 실패하였습니다.");
+                      this.setPopbox_error("카카오 로그인이 실패하였습니다.")
                 });
             },
             fail:(error)=>{
-                 this.$store.commit("alert_Error", "로그인이 실패하였습니다.");
+                  this.setPopbox_error("카카오 로그인이 실패하였습니다.")
             }
         })
     },
@@ -206,16 +227,20 @@ export default {
     },
     //일반 로그인
     async login(){
-      if(!!this.checkEmptyValueAndVerifyRegex(this.id
-              ,/^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/
-              ,"아이디를 입력하지 않았거나 이메일 형식이 아닙니다.")
-        &&!!this.checkEmptyValue(this.pw, "비밀번호를 입력해주세요.")){
+        if(!this.checkEmailRegex(this.email_id)){
+             this.setPopbox_error("올바른 이메일 형식이 아닙니다.")
+            return;
+        }
+        if(!this.checkEmptyValue(this.pw)){
+             this.setPopbox_error("비밀번호를 입력해주세요.")
+            return;
+        }    
 
         await instance({
           url: "http://localhost:2030/login",
           method: "post",
           data:{
-            userId: this.id,
+            userId: this.email_id,
             password: this.pw
           }
         }).then((response)=>{
@@ -225,12 +250,12 @@ export default {
             this.saveTokenAndSetLoginStatus(response.data.data.accessToken,response.data.data.refreshToken)
             window.location.reload()
           }else{
-            this.$store.commit("alert_Warning", "계정이 존재하지 않거나"+"\u00A0" +"이메일 또는 비밀번호가 정확하지 않습니다.");
+            this.setPopbox_error("계정이 존재하지 않거나 이메일 또는 비밀번호가 정확하지 않습니다.") 
           }
         }).catch((error)=>{
-          this.$store.commit("alert_Error", "로그인이 실패하였습니다.");
+            this.setPopbox_error("로그인에 실패하였습니다.")
         });
-      }
+      
     },
     //AccessToken, RefreshToken sessionStorage에 저장 && 로그인 상태값 true
     saveTokenAndSetLoginStatus(accessToken, refreshToken){
@@ -239,24 +264,51 @@ export default {
         this.$store.commit("setIsLogin", true);
         this.isLogin = true;
     },
-    //일반 로그인시 빈값 체크
-    checkEmptyValue(item, message){
+    //일반 로그인시 패스워드 빈값 체크
+    checkEmptyValue(item){
       if(!item){
-        return this.$store.commit("alert_Warning", message)
+        return false
       }else{
         return true
       }
     },
-    //일바 로그인시 정규표현식 체크
-    checkEmptyValueAndVerifyRegex(item, regex ,message){
-      this.checkEmptyValue(item, message);
+    //일바 로그인시 이메일 정규표현식 체크
+    checkEmailRegex(item){
+      const regex = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/
       if(!regex.test(item)){
-        return this.$store.commit("alert_Warning", message)
+        return false
       }else{
         return true
       }
     },
-
+    //setPopBox
+    setPopbox_completeSignup(){
+        this.pop_box =  { 
+            "icon": "check-circle-fill",
+            "border": "0.13rem solid #7dc38d",
+            "background-color": "#7dc38d",
+            message: "회원가입이 완료되었습니다!"
+        }
+        this.showHeaderBox = true
+    },
+    setPopbox_error(message){
+        this.showHeaderBox = false
+        this.pop_box = {
+            "icon":"exclamation-triangle-fill",
+            "border": "0.13rem solid #F55151",
+            "background-color": "#F55151",
+            message: message
+        }
+        this.showHeaderBox = true
+    },
+    idBlur(){
+        if(!this.checkEmailRegex(this.email_id)){
+             this.setPopbox_error("올바른 이메일 형식이 아닙니다.")
+            return;
+        }else{
+            this.showHeaderBox = false;
+        }
+    }
 
 
 
@@ -265,6 +317,18 @@ export default {
 </script>
 </script>
 <style scoped>
+    .pop_box{
+        width: 60%;
+        height: 8rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 1rem;
+        margin-bottom: 0.9rem;
+        color:  white;
+        box-shadow:  10px 10px 20px rgba(0, 0, 0, 0.1);
+    }
+  
     .login_constructor{
          padding-top: 6.5rem; 
          padding-bottom: 2.5rem;
@@ -276,15 +340,27 @@ export default {
     }
 
     .login_container{
-        width: 80%;
+        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
+        width: 60%;
         border: 0.13rem solid rgb(182, 175, 175);
         background-color: white;
         padding: 6.5rem 4.5rem;
         padding-bottom: 2.5rem;
     }
-
-
-
+    .sign{
+       display:flex; 
+       justify-content: center; 
+       margin:2rem 0rem; 
+       width:100%; 
+       font-size: 1.3rem;
+    }
+    .button{
+        height: 4.8rem; 
+        font-size:1.8rem; 
+        font-weight:bold; 
+        width: 39rem; 
+        margin:2rem 0rem !important;
+    }
     .socialIconLine{
         display: flex;
         flex-direction: row;
@@ -295,8 +371,12 @@ export default {
     .socialIcon{
         margin: 0rem 0.8rem;
         width: 3.5rem;
+        border:0; padding: 0; 
+        margin: 0rem 2rem;
     }
-
+    .icon_image{
+        height: 4.25rem; width: 4.25rem;
+    }
     .orElse {
         display: flex;
         flex-basis: 100%;
@@ -315,5 +395,71 @@ export default {
         font-size: 0px;
         line-height: 0px;
         margin: 0px 16px;
+    }
+    .label{
+        font-size: 1.8rem; width: 39rem; font-weight:bold;
+    }
+    .b_icon{
+        width: 2rem; height: 2rem; margin: 0 0.65rem;
+    }
+    .input{
+        height: 4.5rem; font-size: 2rem; width: 10rem !important;
+    }
+
+    .slide-fade-enter-active {
+        transition: all .5s ease !important;
+    }
+    .slide-fade-leave-active {
+        transition: all .5s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-to
+    /* .slide-fade-leave-active below version 2.1.8 */ {
+        transform: translateY(10px);
+    opacity: 0;
+    }
+
+
+
+
+    @media (max-width: 798px) {
+        .login_container{
+            width: 95%;
+            padding: 3rem 1rem;
+        }
+        .label{
+            width: 19rem;
+            font-size: 1rem;
+        }
+        .pop_box{
+            font-size: 1rem;
+            width: 95%;
+            height: 5rem;
+        }
+        .input{
+            font-size: 1rem;
+            height: 3rem;
+        }
+        .b_icon{
+            width: 1.5rem;
+        }
+        .input-group-text{
+             width: 3rem;
+             padding: 0;
+        }
+        .button{
+            height: 3rem; 
+            font-size:1.2rem; 
+        }
+        .sign{
+            font-size: 1rem;
+        }
+        .icon_image{
+            width: 3.5rem;
+            height: 3.5rem;
+        }
+        .socialIcon{
+            margin: 1rem;
+        }
+        
     }
 </style>
